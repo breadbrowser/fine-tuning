c050d06640b08c7ed3b7c302eec22ca9205717ed
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2023-06-20 11:16:36 -0400

    Update run_clm.py

--- run_clm.py
+++ run_clm.py
@@ -1,7 +1,7 @@
 token="breadlicker45/muse-tokenizer"
 model="decapoda-research/llama-7b-hf"
 dataset_name="breadlicker45/Calorie-dataset"
-collum='data'
+column='data'
 max_length=2048
 learning=3e-6
 
