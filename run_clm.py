4a9229690c013dd117fb9a144b0c2bd656db23e9
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2023-06-20 08:40:11 -0400

    Update run_clm.py

--- run_clm.py
+++ run_clm.py
@@ -12,13 +12,13 @@ from collie import Trainer, CollieConfig, LlamaForCausalLM
 from collie.optim.lomo import Lomo
 from transformers import LlamaTokenizer
 
-config = CollieConfig.from_pretrained("decapoda-research/llama-7b-hf")
+config = CollieConfig.from_pretrained("breadlicker45/llama-test")
 config.ds_config = {
     "fp16": {
         "enabled": True
     }
 }
-model = LlamaForCausalLM.from_pretrained("decapoda-research/llama-7b-hf", config=config)
+model = LlamaForCausalLM.from_pretrained("breadlicker45/llama-test", config=config)
 optimizer = Lomo(model=model, lr=2e-5)
 tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf", add_eos_token=True)
 tokenizer.pad_token_id = 0
